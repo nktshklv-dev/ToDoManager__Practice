eefb50d9a094809a4f0b988ec65f8b2c81c94220
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-25 23:34:34 +0300

    created xib file and some classes

---
+++ ToDoManager__PRACTICE/Controllers/TaskEditController.swift
@@ -0,0 +1,95 @@
+//
+//  TaskEditController.swift
+//  ToDoManager__PRACTICE
+//
+//  Created by Nikita  on 5/25/22.
+//
+
+import UIKit
+
+class TaskEditController: UITableViewController {
+    
+    var taskName: String = ""
+    var taskType: TaskPriority = .normal
+    var taskStatus: TaskStatus = .planned
+    var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void)?
+    @IBOutlet var taskNameField: UITextField!
+    @IBOutlet var taskTypeLabel: UILabel!
+    var stringForTaskType: [TaskPriority: String] = [.important: "Important", .normal: "Ordinary"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        taskNameField.text = taskName
+        taskTypeLabel.text = stringForTaskType[taskType]
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoManager__PRACTICE/TaskCellClass/TaskTypeCell.swift
@@ -0,0 +1,16 @@
+//
+//  TaskTypeCell.swift
+//  ToDoManager__PRACTICE
+//
+//  Created by Nikita  on 5/25/22.
+//
+
+import UIKit
+
+class TaskTypeCell: UITableViewCell {
+
+    
+    @IBOutlet var typeName: UILabel!
+    @IBOutlet var typeDescription: UILabel!
+    
+}
